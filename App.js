24ef3dbab2b09a5289020f626960bd1e91649b63
Kamlesh <sh.kamleshsharma@gmail.com>
2021-02-21 16:10:40 +0530

    [C-0001] : Implementation of Swiper list with local data.

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, StatusBar, View } from 'react-native';
+import MainScreen from './controller/mainScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar barStyle = "dark-content" 
+      animated={true}
+      barStyle= "default"
+      hidden = {false} translucent = {true}/>
+      <MainScreen />
       <StatusBar style="auto" />
     </View>
   );
---
+++ controller/mainScreen.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import SwiperScreen from './swiperScreen';
+
+export default class MainScreen extends Component {
+    render() {
+        return (
+            <SwiperScreen />
+        );
+    }
+}
