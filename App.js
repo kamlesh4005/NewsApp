bcccf71c0be8f2624e5e3fbcf48e7e7c4ad67552
Kamlesh <sh.kamleshsharma@gmail.com>
2021-02-27 22:42:31 +0530

    [C-0002] : Basic Implementation for expo notifications and Rate us onExit()

--- App.js
+++ App.js
@@ -1,18 +1,80 @@
-import React from 'react';
-import { StyleSheet, StatusBar, View } from 'react-native';
+import React,{ useEffect, Component } from 'react';
+import { StyleSheet, StatusBar, View, BackHandler, ToastAndroid, Linking, Alert } from 'react-native';
 import MainScreen from './controller/mainScreen';
+import registerForPushNotifications from './controller/registerForPushNotifications';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle = "dark-content" 
-      animated={true}
-      barStyle= "default"
-      hidden = {false} translucent = {true}/>
-      <MainScreen />
-      <StatusBar style="auto" />
-    </View>
-  );
+let backPressed = 0;
+
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      isloggedin: false,
+      backPressed: 1
+    }
+  }
+
+  useEffect() {
+    registerForPushNotifications();
+  };
+
+  componentDidMount(){
+    BackHandler.addEventListener('hardwareBackPress', this.handleBackButton.bind(this));
+  }
+
+  handleBackButton(){
+    if(backPressed > 0){
+      BackHandler.exitApp();
+      backPressed = 0;
+    }else {
+      backPressed++;
+      Alert.alert(
+        'Rate us',
+        'Would you like to share your review with us? This will help and motivate us a lot.',
+        [
+          {text: 'Sure', onPress: () => this.openStore()},
+          {
+            text: 'Later!',
+            onPress: () => console.log('Later Pressed'),
+            style: 'cancel',
+          },
+          {text: 'Exit', onPress: () => BackHandler.exitApp()},
+        ],
+        {cancelable: false},
+      );
+      //ToastAndroid.show("Press Again To Exit", ToastAndroid.SHORT);
+      setTimeout( () => { backPressed = 0}, 2000);
+      return true;
+    }
+  }
+
+  openStore() {
+    if (Platform.OS != 'ios') {
+      ToastAndroid.show("Please Provide 5 * rating to US....", ToastAndroid.LONG);
+      Linking.openURL(
+        `market://details?id=com.flipkart.android`,
+      ).catch(
+          (err) => alert('Please check for Google Play Store')
+      );
+    } else {
+      Linking.openURL(
+        `itms://itunes.apple.com/in/app/apple-store/${APPLE_STORE_ID}`,
+      ).catch((err) => alert('Please check for the App Store'));
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle = "dark-content" 
+        animated={true}
+        barStyle= "default"
+        hidden = {false} translucent = {true}/>
+        <MainScreen />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ controller/registerForPushNotifications.js
@@ -0,0 +1,31 @@
+//registerForPushNotifications.js
+import * as Notifications from 'expo-notifications'
+import * as Permissions from 'expo-permissions';
+
+const PUSH_ENDPOINT = 'https://radial-holy-list.glitch.me/token';
+
+const registerForPushNotifications = async () => {
+  const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+  if (status !== 'granted') {
+    alert('No notification permissions!');
+    return;
+  }
+
+  // Get the token that identifies this device
+  let token = await Notifications.getExpoPushTokenAsync();
+
+  // POST the token to your backend server from where you can retrieve it to send push notifications.
+  return fetch(PUSH_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      token: {
+        value: token,
+      }
+    }),
+  });
+}
+export default registerForPushNotifications;
